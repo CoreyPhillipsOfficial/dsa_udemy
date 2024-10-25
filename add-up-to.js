bfe8707c7fca923da01b9e060ee1fb0499f3258d
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2024-10-25 11:28:33 -0400

    wrote quick add-up-to function with for loop

---
+++ add-up-to.js
@@ -0,0 +1,11 @@
+// Write a function that calculates the sum of all numbers from 1 up to (and including) come number n.
+
+function addUpTo(n) {
+    let total = 0;
+    for (let i = 0; i <= n; i++) {
+       total += i;  
+    }
+    return total;
+}
+
+console.log(addUpTo(6));
