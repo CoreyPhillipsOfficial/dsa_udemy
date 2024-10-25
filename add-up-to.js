bb30fc8d227fdb8472587c829f5635243c723891
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2024-10-25 11:50:31 -0400

    wrote shorter version of this function

--- add-up-to.js
+++ add-up-to.js
@@ -1,5 +1,4 @@
 // Write a function that calculates the sum of all numbers from 1 up to (and including) come number n.
-
 function addUpTo(n) {
     let total = 0;
     for (let i = 0; i <= n; i++) {
@@ -9,3 +8,13 @@ function addUpTo(n) {
 }
 
 console.log(addUpTo(6));
+console.log(addUpTo(100));
+
+
+// Different way to approach this
+function addUpToShorter(n) {
+    return n * (n + 1) / 2;
+}
+
+console.log(addUpToShorter(6));
+console.log(addUpToShorter(100));
